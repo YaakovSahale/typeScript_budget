7668e5b26b868c0e7a2c3dba728fcaa52e072bef
YaakovSahale <yaakov.sahale@gmail.com>
2023-02-01 09:50:08 +0200

    expanses with percentage

--- src/view/Expanses/Expanses.ts
+++ src/view/Expanses/Expanses.ts
@@ -1,12 +1,20 @@
-import { deleteBudgetItem, getLastBudgetItem } from "../../logic/Budget-utils";
+import {
+  computeSum,
+  deleteBudgetItem,
+  getLastBudgetItem,
+} from "../../logic/Budget-utils";
 import { BudgetType } from "../../logic/enums";
 import { formatNumber } from "../../logic/general-utils";
-import IBudgetItem from "../../logic/interfaces/BudgetItem";
 import { ClassIncExp } from "../enums";
 import { updateHead } from "../Head/Head";
+import { formatFinitePercentage } from "../view-utils";
+import IBudgetItem from "../../logic/interfaces/BudgetItem";
 import "./Expanses.css";
 
 const CLASS_ROOT = "expanses";
+const CLASS_PERCENTAGE = "percentage";
+const CLASS_NUMBER = "NUMBER";
+const CLASS_AMOUNT ='amount'
 
 export const Expanses = () => {
   (window as any).deleteExpanse = deleteExpanse;
@@ -24,11 +32,20 @@ const getExpanseElem = (): HTMLDivElement => {
 };
 
 const createExpanseElem = (item: IBudgetItem): string => {
+  const totalIncome = computeSum(BudgetType.Income);
+  const expanse = item.amount;
+  const percentageElem = `<span class=${CLASS_PERCENTAGE}>${formatFinitePercentage(
+    expanse,
+    totalIncome
+  )}<span>`;
+    const amountElem = `<span class=${CLASS_AMOUNT}>${formatNumber(expanse)}</span>`
+
   const leftElem = `<span class=${ClassIncExp.Left}>${item.description}</span>`;
   const buttonElem = `<span class='delete fa fa-minus-circle' onclick=deleteExpanse('${item.id}')></span>`;
   const rightElem = `<div class=${ClassIncExp.Right}>
-  <span>${formatNumber(item.amount)}</span><span>${buttonElem}</span>
+  <span class=${CLASS_NUMBER}>${amountElem} ${percentageElem}</span><span>${buttonElem}</span>
   </div>`;
+
 
   return `<div class=${ClassIncExp.Item} id=${getDomItemId(
     item.id
